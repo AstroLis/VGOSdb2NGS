3e3385ec801f9f15e7f74b972a48b5a7eb1bf167
ksl <kurdubov@iaaras.ru>
2019-02-26 11:16:18 +0300

    test examples

---
+++ test_linux.sh
@@ -0,0 +1,2 @@
+python read.py --archives-dir "./vgosdb_test_files/"
+python read.py --archive "./vgosdb_test_files/18MAR02XU.tar.gz"
---
+++ test_windows.bat
@@ -0,0 +1,3 @@
+python.exe read.py --archives-dir "./vgosdb_test_files/"
+python.exe read.py --archive "./vgosdb_test_files/18MAR02XU.tar.gz"
+pause -1
